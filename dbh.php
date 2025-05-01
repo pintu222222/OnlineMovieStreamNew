0e52cfc090b9f73cfe42f11f4542bfb85c05bbca
Sanatan Ratha <sanatanratha0@gmail.com>
2025-05-01 16:14:42 +0530

    data base updated

--- dbh.php
+++ dbh.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = mysqli_connect("localhost","root","","sourcecodester_omsdb");
+  $conn = mysqli_connect("localhost","root","","fmdiaries_database");
   if(! $conn ) {
       die('Could not connect: ' . mysqli_error());
    }
