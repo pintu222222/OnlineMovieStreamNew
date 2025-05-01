c3a3837357c70663447d0c6d9f3c31187044aa04
Sanatan Ratha <sanatanratha0@gmail.com>
2025-05-01 16:02:26 +0530

    database updated

--- database/sourcecodester_omsdb.sql
+++ database/sourcecodester_omsdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2020 at 02:08 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 5.6.39
+-- Generation Time: May 01, 2025 at 12:30 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,26 +34,10 @@ CREATE TABLE `movies` (
   `rdate` varchar(5) NOT NULL,
   `runtime` varchar(4) NOT NULL,
   `decription` varchar(100) NOT NULL,
-  `viewers` int(10) DEFAULT '1',
+  `viewers` int(10) DEFAULT 1,
   `imgpath` text NOT NULL,
   `videopath` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `movies`
---
-
-INSERT INTO `movies` (`mid`, `name`, `genre`, `rdate`, `runtime`, `decription`, `viewers`, `imgpath`, `videopath`) VALUES
-(1, 'rampage', 'fiction', '2017', '120', 'animals', 8, 'rampage.jpg', 'RAMPAGE Trailer.mp4'),
-(2, 'black panther', 'fiction', '2017', '140', 'super hero movie', 13, 'black panther.jpg', 'Black Panther.mp4'),
-(3, 'spiderman homecoming', 'fiction', '2018', '110', 'super hero movie', 5, 'spider-man-homecoming.jpg', 'Spider-Man Homecoming.mp4'),
-(4, 'jumanji', 'adventure', '2017', '130', '4 kids stuck in video game', 12, 'jumanji2017.jpg', 'JUMANJI 17.mp4'),
-(5, 'the conjuring', 'horror', '2013', '120', 'ghost house', 1, 'Conjuring.jpg', 'The Conjuring.mp4'),
-(6, 'the conjuring 2', 'horror', '2015', '115', 'cursed family', 1, 'conjuring2.jpg', 'The Conjuring 2.mp4'),
-(7, 'infinity wars ', 'fiction', '2018', '123', 'collaboration of all marvel characters', 5, 'infinity war.jpg', 'Avengers Infinity War.mp4'),
-(8, 's', 's', 's', '', 's', 27, 'Baby Care Website in PHP with Full Source Code.jpg', ''),
-(9, 's', 's', 's', '12', 'sd', 27, 'Online Attendance Management System in PHP with Full Source Code.jpg', 'Attendance Monitoring.mp4'),
-(10, 'sadasdas', 'asd', 'asd', '', 'asd', 19, 'Attendance Monitoring System in Android with Full Source Code.jpg', 'Attendance Monitoring.mp4');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -67,7 +50,7 @@ CREATE TABLE `rating` (
   `mid` int(11) DEFAULT NULL,
   `id` int(11) DEFAULT NULL,
   `rate_count` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -82,19 +65,20 @@ CREATE TABLE `user1` (
   `name` varchar(20) NOT NULL,
   `phone` varchar(10) NOT NULL,
   `email` varchar(25) NOT NULL,
-  `DOB` varchar(10) NOT NULL,
-  `mid` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `DOB` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user1`
 --
 
-INSERT INTO `user1` (`id`, `username`, `passwd`, `name`, `phone`, `email`, `DOB`, `mid`) VALUES
-(1, 'admin@gmail.com', 'admin', 'shubham belgaonkar', '8692849041', 'shubhamb756@gmail.com', '25/04/1998', 7),
-(4, 'soubik@gmail.com', '1234', 'soubik bera', '8622849041', 'soubik@gmail.com', '16/10/1995', 7),
-(5, 'niru@gmail.com', '1234', 'niru lal', '1234287564', 'niru@gmail.com', '16/09/1996', 7),
-(6, 'janobe@gmail.com', 'admin', 's s', '9876565421', 'janobe@gmail.com', '15/01/1995', 7);
+INSERT INTO `user1` (`id`, `username`, `passwd`, `name`, `phone`, `email`, `DOB`) VALUES
+(1, 'Sanatan0', 'Admin', 'Sanatan Ratha', '9861309750', 'sanatanratha0@gmail.com', '01/04/2002'),
+(4, 'soubik@gmail.com', '1234', 'soubik bera', '8622849041', 'soubik@gmail.com', '16/10/1995'),
+(5, 'niru@gmail.com', '12345', 'niranjan behera', '9178854234', 'niru@gmail.com', '16/09/1996'),
+(6, 'janobe@gmail.com', 'admin', 's s', '9876565421', 'janobe@gmail.com', '15/01/1995'),
+(7, 'satyajeet@gmail.com', '1234', 'satyajeet rout', '8144507955', 'satyajeet@gmail.com', '6/03/2002'),
+(12, 'sanuratha0@gmail.com', '1234', 'sanu ratha', '9861309751', 'sanuratha0@gmail.com', '9/08/1991');
 
 --
 -- Indexes for dumped tables
@@ -119,8 +103,7 @@ ALTER TABLE `rating`
 --
 ALTER TABLE `user1`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD KEY `mid` (`mid`);
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -130,7 +113,7 @@ ALTER TABLE `user1`
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `mid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `mid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `rating`
@@ -142,7 +125,7 @@ ALTER TABLE `rating`
 -- AUTO_INCREMENT for table `user1`
 --
 ALTER TABLE `user1`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
